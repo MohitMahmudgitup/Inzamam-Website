e7d9481c229ee45de1c903771a4396180d9cb84c
MohitMahmudgitup <mohitmahmud25@gmail.com>
2024-08-28 01:14:06 +0600

    pdf update

--- src/components/Navber/Header.jsx
+++ src/components/Navber/Header.jsx
@@ -9,9 +9,10 @@ import { IoClose } from "react-icons/io5";
 import { SiCodersrank } from "react-icons/si";
 import { IoCloudDownloadSharp } from "react-icons/io5";
 import { FaGithub } from "react-icons/fa6";
+import pdf from "../../pdf/MohitMahmud.pdf"
 
 function Header() {
-  // const Logo = "</> "
+
 
 
   useGSAP(()=>{
@@ -68,7 +69,7 @@ function Header() {
                   <Link to={""}           className='f3 px-[13px] py-[6px]  font-medium rounded-full border-[1px] text-[12px] castom-nav-item'>HOME</Link>
                   <Link to={"/about"}     className='f3 px-[13px] py-[6px]  font-medium rounded-full border-[1px] text-[12px] castom-nav-item'>ABOUT</Link>
                   <Link to={"/contectUs"} className='f3 px-[13px] py-[6px]  font-medium rounded-full border-[1px] text-[12px] castom-nav-item'>CONTECT US</Link>
-                  <a href='../../pdf/MohitMahmud.pdf' download="MohitMahmud.pdf">
+                  <a download="MohitMahmud" href={pdf} >
                     <div className='f3 p-[10px]  font-medium rounded-full border-[1px] text-[12px]  castom-nav-item'>
                     <IoCloudDownloadSharp />
                     </div>
@@ -96,8 +97,8 @@ function Header() {
                   <NavLink to={"/about"}     className='navkink f1 py-2 px-10    rounded-3xl castom-c-a3a3a3 text-5xl '>About</NavLink>
                   <NavLink to={"/contectUs"} className='navkink f1 py-2 px-10    rounded-3xl castom-c-a3a3a3 text-5xl '>Contact Us</NavLink>
                   <div className=' flex absolute bottom-4 right-3 gap-4 justify-center'>
-                  <a href='../../pdf/MohitMahmud.pdf' download="MohitMahmud.pdf">
-                    <div className='f3 p-[10px]  font-medium rounded-full border-[1px] text-[12px]  castom-nav-item'>
+                  <a download="MohitMahmud" href={pdf} >
+                    <div  className='f3 p-[10px]  font-medium rounded-full border-[1px] text-[12px]  castom-nav-item'>
                     <IoCloudDownloadSharp className='w-6 h-6 ' />
                     </div>
                   </a>
