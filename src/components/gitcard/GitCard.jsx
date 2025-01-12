afc072dd00cc5044ce0be8f9a062481d81ea636e
MohitMahmudgitup <mohitmahmud25@gmail.com>
2025-01-12 13:25:12 +0600

    update again time

--- src/components/gitcard/GitCard.jsx
+++ src/components/gitcard/GitCard.jsx
@@ -3,18 +3,18 @@ import { components } from "../../jsFile/index.js"; // Ensure components are imp
 
 const GitCard = () => {
   return (
-    <div className="w-full   px-6 py-5 sm:py-16 rounded-2xl  flex flex-1 items-center">
+    <div className="w-full    py-5 sm:py-16 rounded-2xl  flex flex-1 items-center">
       <div className="flex w-full flex-col justify-between items-center gap-4 ">
         {components.map((item, index) => (
           // If item.gits is an array, loop through it as well
           item.gits.map((git, gitIndex) => (
-            <div className="w-full p-6 rounded-2xl bg-zinc-950 shadow-lg flex justify-between sm:flex-row flex-col sm:gap-0 gap-8  ">
-            <div className='w-80 flex justify-start items-start '>
+            <div className="w-full   p-6 rounded-2xl bg-zinc-950 shadow-lg flex justify-between sm:flex-row flex-col sm:gap-0 gap-8  ">
+            <div className='sm:w-80 flex justify-start items-start '>
             <h1 className="text-lg font-bold text-white">{git.title}</h1>
 
             </div>
-            <div className='w-96 flex justify-start items-start '>
-            <p className="text-sm text-gray-100 mt-2">{git.description}</p>
+            <div className='sm:w-96 sm:h-full h-40    '>
+            <p className=" text-sm font-normal  text-gray-400 ">{git.description}</p>
             </div>
             <div className='sm:w-40  flex justify-end items-center  '>
 
